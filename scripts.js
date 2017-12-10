9986ea6527cfad249807cab2513a87d85d7dbcad
Linghan Li <32752716+Mivialll@users.noreply.github.com>
2017-12-09 20:11:22 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,18 @@
 $("document").ready(function(){
+	
 	$("#btnSubmit").click(function(){
+		
 		if (valid())
 		$("#res").html("$:  " + (Math.round(money( $("#txtSalary").val(), $("#txtLoanPayment").val(), $("#txtPayOffTime").val(), $("#txtFixedExpenses").val()) * 100) / 100))
 		else
 		$("#res").html("<div class='text-danger'>Please Enter Valid Values</div>")
 
+		
 		function money(salary, loan, years, expenses) {
 		    return salary - loan / years - expenses;
 		}
 
+		
 		function valid()
 		{
 			if ($.isNumeric($("#txtSalary").val()) && $.isNumeric($("#txtLoanPayment").val()) &&  $.isNumeric($("#txtPayOffTime").val()) && $.isNumeric($("#txtFixedExpenses").val()))
